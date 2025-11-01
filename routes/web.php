22cd339ca8a7883d0c579c1684dfec1ec8413724
Anshari06 <ansharishidqi@gmail.com>
2025-11-02 00:16:01 +0700

    fix(login): buat route ke halaman login

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\ManageUserController;
 // dashboard route handled by controller
 Route::get('/', [DashboardController::class, 'index']);
 
+Route::get('/login', function () {
+    return view('login');
+});
+
 // Manage user
 Route::get('/manage_user', [ManageUserController::class, 'index']);
 
